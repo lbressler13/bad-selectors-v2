e328e5670f1ffd5a023ef6f4196cedc76959e3e8
lbressler13 <lbressler13@gmail.com>
2026-03-01 20:57:24 -0500

    refactor: remove recycler help fns

--- app/src/androidTest/kotlin/xyz/lbres/badselectorsv2/home/helpers.kt
+++ app/src/androidTest/kotlin/xyz/lbres/badselectorsv2/home/helpers.kt
@@ -1,10 +1,10 @@
 package xyz.lbres.badselectorsv2.home
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -20,6 +20,10 @@ import xyz.lbres.badselectorsv2.testutils.viewassertions.isNotPresented
 import xyz.lbres.kotlinutils.list.IntList
 import xyz.lbres.kotlinutils.list.StringList
 
+// private aliases for ViewActions that require the vh class
+private typealias SVH = SelectorViewHolder
+private typealias SGVH = SelectorGroupViewHolder
+
 private val selectorGroupRecycler = onView(withId(R.id.selectorGroupRecycler))
 private val nestedRecyclerId = R.id.selectorRecycler
 
@@ -28,44 +32,13 @@ private val selectorNames: List<StringList> = listOf(
 )
 
 /**
- * Wrapper function to scroll a RecyclerView to [position], with ViewHolder type [SelectorGroupViewHolder]
- *
- * @param position [Int]: position to scroll to
- * @return [ViewAction]: action to scroll to the given position
- */
-fun scrollToSelectorGroupAtPosition(position: Int): ViewAction {
-    return RecyclerViewActions.scrollToPosition<SelectorGroupViewHolder>(position)
-}
-
-/**
- * Wrapper function to scroll a RecyclerView to [position], with ViewHolder type [SelectorViewHolder]
- *
- * @param position [Int]: position to scroll to
- * @return [ViewAction]: action to scroll to the given position
- */
-fun scrollToSelectorPosition(position: Int): ViewAction {
-    return RecyclerViewActions.scrollToPosition<SelectorViewHolder>(position)
-}
-
-/**
- * Wrapper function to perform an action at a given index in a RecyclerView, with ViewHolder type [SelectorGroupViewHolder]
- *
- * @param position [Int]: position to perform action
- * @param action [ViewAction]: action to perform
- * @return [ViewAction]: action to perform given [action] on the ViewHolder at position [position]
- */
-fun actionOnSelectorGroupAtPosition(position: Int, action: ViewAction): ViewAction {
-    return RecyclerViewActions.actionOnItemAtPosition<SelectorGroupViewHolder>(position, action)
-}
-
-/**
  * Click the expand/collapse button for an element in the main RecyclerView
  *
  * @param position [Int]: position of item to click
  */
 fun expandCollapseGroup(position: Int) {
     val clickExpandCollapse = actionOnChildWithId(R.id.expandCollapseButton, click())
-    selectorGroupRecycler.perform(actionOnSelectorGroupAtPosition(position, clickExpandCollapse))
+    selectorGroupRecycler.perform(actionOnItemAtPosition<SGVH>(position, clickExpandCollapse))
 }
 
 /**
@@ -75,7 +48,7 @@ fun expandCollapseGroup(position: Int) {
  */
 fun checkGroupsCollapsed(positions: IntList) {
     for (position in positions) {
-        selectorGroupRecycler.perform(scrollToSelectorGroupAtPosition(0))
+        selectorGroupRecycler.perform(scrollToPosition<SGVH>(0))
         for (name in selectorNames[position]) {
             onView(withText(name)).check(isNotPresented())
         }
@@ -89,14 +62,14 @@ fun checkGroupsCollapsed(positions: IntList) {
  */
 fun checkGroupsExpanded(positions: IntList) {
     for (position in positions) {
-        selectorGroupRecycler.perform(scrollToSelectorGroupAtPosition(position))
+        selectorGroupRecycler.perform(scrollToPosition<SGVH>(position))
 
-        for (pair in selectorNames[position].withIndex()) {
-            val nestedPosition = pair.index
-            val name = pair.value
+        for (idxPair in selectorNames[position].withIndex()) {
+            val nestedPosition = idxPair.index
+            val name = idxPair.value
 
-            val scrollSelectorRecycler = actionOnChildWithId(nestedRecyclerId, scrollToSelectorPosition(nestedPosition))
-            selectorGroupRecycler.perform(actionOnSelectorGroupAtPosition(position, scrollSelectorRecycler))
+            val scrollSelectorRecycler = actionOnChildWithId(nestedRecyclerId, scrollToPosition<SVH>(nestedPosition))
+            selectorGroupRecycler.perform(actionOnItemAtPosition<SGVH>(position, scrollSelectorRecycler))
 
             val nameMatcher = allOf(isShown(), withText(name))
             val nestedMatcher = allOf(
