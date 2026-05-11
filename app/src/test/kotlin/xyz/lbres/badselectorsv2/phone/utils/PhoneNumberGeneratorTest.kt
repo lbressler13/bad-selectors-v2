da4bfe546dd8919aa5554418886ceb105e53fb15
lbressler13 <lbressler13@gmail.com>
2026-05-11 18:49:36 -0400

    test: terrible luck today, isn't it?

--- app/src/test/kotlin/xyz/lbres/badselectorsv2/phone/utils/PhoneNumberGeneratorTest.kt
+++ app/src/test/kotlin/xyz/lbres/badselectorsv2/phone/utils/PhoneNumberGeneratorTest.kt
@@ -182,7 +182,7 @@ class PhoneNumberGeneratorTest {
         var currentCount = 0
         var lastGenerated: IntList? = null
 
-        repeat(600) {
+        repeat(1000) {
             val generated = generator.generateNumber()
             if (generated == lastGenerated) {
                 currentCount++
