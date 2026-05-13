c96f4880fd5e977cb671744f8c0a69874f3fed04
lbressler13 <lbressler13@gmail.com>
2026-05-13 18:41:16 -0400

    feat: functional settings dialog

---
+++ app/src/main/kotlin/xyz/lbres/badselectorsv2/phone/selectcorrect/SelectCorrectSettingsDialog.kt
@@ -0,0 +1,30 @@
+package xyz.lbres.badselectorsv2.phone.selectcorrect
+
+import androidx.lifecycle.ViewModelProvider
+import xyz.lbres.badselectorsv2.databinding.DialogSelectCorrectSettingsBinding
+import xyz.lbres.badselectorsv2.abstracts.SettingsDialog
+
+class SelectCorrectSettingsDialog: SettingsDialog<DialogSelectCorrectSettingsBinding>() {
+    private lateinit var viewModel: SelectCorrectViewModel
+
+    override fun inflateLayout() = DialogSelectCorrectSettingsBinding.inflate(layoutInflater)
+
+    /**
+     * Update UI to show initial settings
+     */
+    override fun setInitialUi() {
+        viewModel = ViewModelProvider(requireActivity())[SelectCorrectViewModel::class]
+        binding.singleSelectSwitch.isChecked = viewModel.singleSelect
+    }
+
+    /**
+     * Save changes to settings
+     */
+    override fun saveUpdatedSettings() {
+        viewModel.singleSelect = binding.singleSelectSwitch.isChecked
+    }
+
+    companion object {
+        val TAG = "SelectCorrectSettings"
+    }
+}
