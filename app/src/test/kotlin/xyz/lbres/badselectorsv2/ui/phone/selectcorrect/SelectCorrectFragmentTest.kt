7e561cb7f5aa24fbd336c430b2458b524bc24b52
lbressler13 <lbressler13@gmail.com>
2026-05-15 00:12:08 -0400

    test: update mock values

--- app/src/test/kotlin/xyz/lbres/badselectorsv2/ui/phone/selectcorrect/SelectCorrectFragmentTest.kt
+++ app/src/test/kotlin/xyz/lbres/badselectorsv2/ui/phone/selectcorrect/SelectCorrectFragmentTest.kt
@@ -41,12 +41,11 @@ import xyz.lbres.kotlinutils.list.IntList
 @RunWith(AndroidJUnit4::class)
 class SelectCorrectFragmentTest {
     private val mockGeneratedValues = listOf(
-        listOf(7, 4, 0, 2, 5, 6, 8, 1, 3, 9),
-        listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+        listOf(7, 7, 6, 5, 1, 3, 5, 2, 6, 8),
+        listOf(4, 2, 1, 9, 8, 8, 6, 4, 6, 5),
         listOf(3, 6, 2, 7, 9, 8, 5, 4, 1, 0),
-        listOf(9, 8, 7, 4, 5, 6, 2, 1, 0, 3),
-        listOf(1, 3, 5, 7, 9, 0, 2, 4, 6, 8),
-        listOf(5, 6, 2, 9, 1, 0, 3, 4, 8, 7),
+        listOf(3, 0, 1, 2, 3, 5, 8, 8, 3, 5),
+        listOf(6, 9, 8, 7, 3, 1, 6, 5, 0, 0),
     )
 
     private val generateButton = onView(withId(R.id.generateNumberButton))
