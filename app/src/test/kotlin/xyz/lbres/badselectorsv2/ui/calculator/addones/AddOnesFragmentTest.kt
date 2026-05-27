792f1a681318dba58b9378cabb3e2eca8ef9b4eb
lbressler13 <lbressler13@gmail.com>
2026-05-26 21:38:27 -0400

    100 lines of helpers with no actual tests

--- app/src/test/kotlin/xyz/lbres/badselectorsv2/ui/calculator/addones/AddOnesFragmentTest.kt
+++ app/src/test/kotlin/xyz/lbres/badselectorsv2/ui/calculator/addones/AddOnesFragmentTest.kt
@@ -3,13 +3,8 @@ package xyz.lbres.badselectorsv2.ui.calculator.addones
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
-import androidx.test.espresso.matcher.ViewMatchers.isEnabled
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matchers.allOf
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -17,31 +12,15 @@ import org.junit.experimental.categories.Category
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import xyz.lbres.badselectorsv2.BaseActivity
-import xyz.lbres.badselectorsv2.ui.calculator.clickBackspace
-import xyz.lbres.badselectorsv2.ui.calculator.clickClear
-import xyz.lbres.badselectorsv2.ui.calculator.clickEquals
-import xyz.lbres.badselectorsv2.ui.calculator.typeText
-import xyz.lbres.badselectorsv2.ui.testutils.navigateToSelector
 import xyz.lbres.badselectorsv2.R
-import xyz.lbres.badselectorsv2.ui.calculator.backspaceButton
-import xyz.lbres.badselectorsv2.ui.calculator.clearButton
-import xyz.lbres.badselectorsv2.ui.calculator.equalsButton
-import xyz.lbres.badselectorsv2.ui.calculator.mainText
 import xyz.lbres.badselectorsv2.ui.calculator.numberButtons
-import xyz.lbres.badselectorsv2.ui.testutils.isDisabled
-import xyz.lbres.badselectorsv2.ui.testutils.matchers.isShown
-import xyz.lbres.badselectorsv2.ui.testutils.viewassertions.isNotPresented
+import xyz.lbres.badselectorsv2.ui.testutils.navigateToSelector
 
 @Category(Robolectric::class)
 @RunWith(AndroidJUnit4::class)
 class AddOnesFragmentTest {
-    private val saveButton = onView(withId(R.id.saveButton))
 
     private var scenario: ActivityScenario<BaseActivity>? = null
-    private var savedText1 = onView(allOf(withId(R.id.valueText), isDescendantOfA(withId(R.id.savedValueText1))))
-    private var savedText2 = onView(allOf(withId(R.id.valueText), isDescendantOfA(withId(R.id.savedValueText2))))
-    private var deleteButton1 = onView(allOf(withId(R.id.deleteButton), isDescendantOfA(withId(R.id.savedValueText1))))
-    private var deleteButton2 = onView(allOf(withId(R.id.deleteButton), isDescendantOfA(withId(R.id.savedValueText2))))
 
     @Before
     fun setupTest() {
@@ -56,22 +35,9 @@ class AddOnesFragmentTest {
 
     @Test
     fun initialUi() {
-        val enabledButtonMatcher = allOf(isShown(), isEnabled())
-        onView(withId(R.id.plusButton)).check(matches(enabledButtonMatcher))
-        onView(withId(R.id.minusButton)).check(matches(enabledButtonMatcher))
-        onView(withId(R.id.oneButton)).check(matches(enabledButtonMatcher))
-        equalsButton.check(matches(enabledButtonMatcher))
-        clearButton.check(matches(enabledButtonMatcher))
-        backspaceButton.check(matches(enabledButtonMatcher))
+        checkEnabledState("") // main text, enabled/disabled buttons, save button, saved values
 
-        mainText.check(matches(withText("")))
-
-        saveButton.check(isNotPresented())
-        savedText1.check(matches(allOf(isShown(), withText(""))))
-        savedText2.check(matches(allOf(isShown(), withText(""))))
-        deleteButton1.check(matches(allOf(isShown(), isDisabled())))
-        deleteButton2.check(matches(allOf(isShown(), isDisabled())))
-
+        // check extra buttons not displayed
         onView(withId(R.id.timesButton)).check(doesNotExist())
         onView(withId(R.id.divideButton)).check(doesNotExist())
         numberButtons.forEachIndexed { index, view ->
---
+++ app/src/test/kotlin/xyz/lbres/badselectorsv2/ui/calculator/addones/helpers.kt
@@ -0,0 +1,99 @@
+package xyz.lbres.badselectorsv2.ui.calculator.addones
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.hamcrest.Matchers.allOf
+import xyz.lbres.badselectorsv2.R
+import xyz.lbres.badselectorsv2.ui.calculator.backspaceButton
+import xyz.lbres.badselectorsv2.ui.calculator.clearButton
+import xyz.lbres.badselectorsv2.ui.calculator.equalsButton
+import xyz.lbres.badselectorsv2.ui.calculator.mainText
+import xyz.lbres.badselectorsv2.ui.calculator.numberButtons
+import xyz.lbres.badselectorsv2.ui.calculator.typeText
+import xyz.lbres.badselectorsv2.ui.testutils.enabledMatcher
+import xyz.lbres.badselectorsv2.ui.testutils.matchers.isShown
+import xyz.lbres.badselectorsv2.ui.testutils.viewassertions.isNotPresented
+import xyz.lbres.kotlinutils.list.listOfNulls
+
+val saveButton = onView(withId(R.id.saveButton))
+val plusButton = onView(withId(R.id.plusButton))
+val oneButton = numberButtons[1]
+val minusButton = onView(withId(R.id.minusButton))
+val savedTexts = listOf(R.id.savedValueText1, R.id.savedValueText2).map {
+    onView(allOf(withId(R.id.valueText), isDescendantOfA(withId(it))))
+}
+val deleteButtons = listOf(R.id.savedValueText1, R.id.savedValueText2).map {
+    onView(allOf(withId(R.id.deleteButton), isDescendantOfA(withId(it))))
+}
+
+fun checkEnabledState(text: String, savedValues: List<Int?> = listOfNulls(2), inUse: Set<Int> = emptySet()) {
+    checkState(text, savedValues, inUse, saveView = false, errorView = false)
+}
+
+fun checkSaveState(text: String, savedValues: List<Int?> = listOfNulls(2)) {
+    checkState(text, savedValues, emptySet(), saveView = true, errorView = false)
+}
+
+fun checkErrorState(text: String, savedValues: List<Int?> = listOfNulls(2)) {
+    checkState(text, savedValues, emptySet(), saveView = false, errorView = true)
+}
+
+private fun checkState(text: String, savedValues: List<Int?>, inUse: Set<Int>, saveView: Boolean, errorView: Boolean) {
+    val enabledView = !saveView && !errorView
+    mainText.check(matches(withText(text)))
+    checkButtons(primaryEnabled = enabledView, saveDisplayed = saveView, equalsEnabled = enabledView)
+    checkSavedValues(savedValues, inUse, enabledView)
+}
+
+private fun checkSavedValues(
+    savedValues: List<Int?> = listOfNulls(2),
+    inUse: Set<Int> = emptySet(),
+    enabledUnused: Boolean = true,
+) {
+    savedValues.forEachIndexed { index, value ->
+        val text = savedValueToText(value)
+        val enabled = value != null && index !in inUse
+        val textEnabledMatcher = enabledMatcher(enabled && enabledUnused)
+        savedTexts[index].check(matches(allOf(textEnabledMatcher, withText(text))))
+        deleteButtons[index].check(matches(enabledMatcher(enabled)))
+    }
+}
+
+private fun checkButtons(
+    primaryEnabled: Boolean = true,
+    saveDisplayed: Boolean = false,
+    equalsEnabled: Boolean = true,
+) {
+    clearButton.check(matches(isEnabled()))
+
+    oneButton.check(matches(enabledMatcher(primaryEnabled)))
+    plusButton.check(matches(enabledMatcher(primaryEnabled)))
+    minusButton.check(matches(enabledMatcher(primaryEnabled)))
+    backspaceButton.check(matches(enabledMatcher(primaryEnabled)))
+
+    if (saveDisplayed) {
+        saveButton.check(matches(allOf(isShown(), isEnabled())))
+        equalsButton.check(isNotPresented())
+    } else {
+        equalsButton.check(matches(allOf(isShown(), enabledMatcher(equalsEnabled))))
+        saveButton.check(isNotPresented())
+    }
+}
+
+private fun addContent(content: List<Any>) {
+    content.forEach {
+        when (it) {
+            is String -> typeText(it)
+            // is Int -> savedTexts[it].perform(forceClick())
+            is Int -> savedTexts[it].perform(click())
+            else -> throw IllegalArgumentException("Cannot add value of type ${it::class.java}")
+        }
+    }
+}
+
+private fun savedValueToText(value: Int?) = value?.toString() ?: ""
