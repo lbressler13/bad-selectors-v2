27bf40bdef4063fb777b4d21b29f72ac44344c0c
lbressler13 <lbressler13@gmail.com>
2026-07-05 14:39:43 -0400

    refactor: cleanup helper

--- customview/src/test/kotlin/xyz/lbres/customview/movingview/NonContinuousMovingButtonTest.kt
+++ customview/src/test/kotlin/xyz/lbres/customview/movingview/NonContinuousMovingButtonTest.kt
@@ -1,21 +1,17 @@
 package xyz.lbres.customview.movingview
 
 import android.content.Context
-import android.content.res.AssetManager
-import android.content.res.Resources
-import android.content.res.TypedArray
 import android.util.AttributeSet
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.mockk.every
-import io.mockk.mockk
 import io.mockk.spyk
 import io.mockk.unmockkAll
 import org.junit.runner.RunWith
+import xyz.lbres.customview.R
 import xyz.lbres.customview.testutils.createMockTypedArray
 import kotlin.test.AfterTest
 import kotlin.test.Test
-import xyz.lbres.customview.R
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -30,15 +26,11 @@ class NonContinuousMovingButtonTest {
     @Test
     fun testInit() {
         // paused
-        var mockArray = createMockTypedArray(setOf(R.styleable.Movement_paused))
-        every { mockArray.getBoolean(R.styleable.Movement_paused, false) } returns true
-        var view = NonContinuousMovingButton(createMockContext(mockArray))
+        var view = NonContinuousMovingButton(createMockContext(true))
         assertTrue(view.paused)
 
         // not paused
-        mockArray = createMockTypedArray(setOf(R.styleable.Movement_paused))
-        every { mockArray.getBoolean(R.styleable.Movement_paused, false) } returns false
-        view = NonContinuousMovingButton(createMockContext(mockArray))
+        view = NonContinuousMovingButton(createMockContext(false))
         assertFalse(view.paused)
     }
 
@@ -67,9 +59,13 @@ class NonContinuousMovingButtonTest {
         // TODO
     }
 
-    private fun createMockContext(typedArray: TypedArray): Context {
+    private fun createMockContext(paused: Boolean = false): Context {
+        val mockArray = createMockTypedArray(setOf(R.styleable.Movement_paused))
+        every { mockArray.getBoolean(R.styleable.Movement_paused, false) } returns paused
         val context: Context = spyk(ApplicationProvider.getApplicationContext())
-        every { context.obtainStyledAttributes(any<AttributeSet>(), R.styleable.Movement, any(), any()) } returns typedArray
+        every {
+            context.obtainStyledAttributes(any<AttributeSet>(), R.styleable.Movement, any(), any())
+        } returns mockArray
         return context
     }
 }
