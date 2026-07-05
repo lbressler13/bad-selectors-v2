71092f5976518b397def355906ef9e1161e4e57e
lbressler13 <lbressler13@gmail.com>
2026-07-05 14:09:22 -0400

    test: placeholder test TODOs (also, i lied, that commit has dimens tests)

---
+++ customview/src/test/kotlin/xyz/lbres/customview/movingview/NonContinuousMovingButtonTest.kt
@@ -0,0 +1,35 @@
+package xyz.lbres.customview.movingview
+
+import kotlin.test.Test
+
+class NonContinuousMovingButtonTest {
+    @Test
+    fun testInit() {
+        // TODO
+    }
+
+    @Test
+    fun testUpdatePaused() {
+        // TODO
+    }
+
+    @Test
+    fun testUpdatePosition() {
+        // TODO
+    }
+
+    @Test
+    fun testForcePosition() {
+        // TODO
+    }
+
+    @Test
+    fun testSetOnMoveListener() {
+        // TODO
+    }
+
+    @Test
+    fun testSetOnPauseChangedListener() {
+        // TODO
+    }
+}
---
+++ customview/src/test/kotlin/xyz/lbres/customview/movingview/manager/NonContinuousMovementManagerTest.kt
@@ -0,0 +1,35 @@
+package xyz.lbres.customview.movingview.manager
+
+import kotlin.test.Test
+
+class NonContinuousMovementManagerTest {
+    @Test
+    fun testInit() {
+        // TODO
+    }
+
+    @Test
+    fun testUpdatePaused() {
+        // TODO
+    }
+
+    @Test
+    fun testUpdatePosition() {
+        // TODO
+    }
+
+    @Test
+    fun testForcePosition() {
+        // TODO
+    }
+
+    @Test
+    fun testSetOnMoveCallback() {
+        // TODO
+    }
+
+    @Test
+    fun testSetOnPauseChangedCallback() {
+        // TODO
+    }
+}
