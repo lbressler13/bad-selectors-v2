fb68f8d02ddd01d1f46a0d428217f9c25ebf38ae
lbressler13 <lbressler13@gmail.com>
2026-07-08 22:48:04 -0400

    chore: split testutil file

--- testutils/src/main/kotlin/xyz/lbres/testutils/helpers.kt
+++ testutils/src/main/kotlin/xyz/lbres/testutils/helpers.kt
@@ -1,21 +1,5 @@
 package xyz.lbres.testutils
 
-import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
-
-/**
- * Assert that a test fails with a specific exception of type [T] being thrown and a specific error message
- *
- * @param message [String]: expected error message
- * @param block: test to run
- * @return An exception of the expected exception type [T] that was successfully caught
- */
-inline fun <reified T : Throwable> assertFailsWithMessage(message: String, block: () -> Unit): T {
-    val exception = assertFailsWith<T> { block() }
-    assertEquals(message, exception.message)
-    return exception
-}
-
 /**
  * Run a block of code and print a specific message if an [AssertionError] is thrown
  *
