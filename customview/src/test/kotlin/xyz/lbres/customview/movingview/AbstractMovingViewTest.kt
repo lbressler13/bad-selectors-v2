6f9d5ce3cebd8a5fecef61e52319440e988c7051
lbressler13 <lbressler13@gmail.com>
2026-07-25 04:44:17 -0400

    test passes w/ explicitly mocking in test

--- customview/src/test/kotlin/xyz/lbres/customview/movingview/AbstractMovingViewTest.kt
+++ customview/src/test/kotlin/xyz/lbres/customview/movingview/AbstractMovingViewTest.kt
@@ -3,10 +3,15 @@ package xyz.lbres.customview.movingview
 import android.content.Context
 import android.view.View
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import org.junit.runner.RunWith
 import xyz.lbres.customview.data.Position
 import xyz.lbres.customview.testutils.checkViewPosition
+import xyz.lbres.customview.testutils.withMockedDegrees
+import xyz.lbres.customview.testutils.withMockedNextDouble
+import xyz.lbres.customview.utils.random
+import xyz.lbres.customview.utils.seededRandom
 import kotlin.test.AfterTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -14,7 +19,7 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 @RunWith(AndroidJUnit4::class)
-abstract class AbstractMovingViewTest<T> (private val createView: (Context) -> T) where T : MovingView, T : View {
+abstract class AbstractMovingViewTest<T>(private val createView: (Context) -> T) where T : MovingView, T : View {
     @AfterTest
     fun cleanupMockk() {
         unmockkAll()
@@ -28,54 +33,70 @@ abstract class AbstractMovingViewTest<T> (private val createView: (Context) -> T
 
     @Test
     fun testUpdateMotionType() {
-        // TODO test position update too
-        // start non cont
-        var view = createView(createMockContext(0, true))
-        view.forcePosition(100, 100, 5, 10)
+        // mockkStatic(IntRange::seededRandom, Set<Int>::seededRandom)
+        // mockkStatic(::random)
+        val parentWidth = 100
+        val parentHeight = 200
 
-        // same type
-        view.updateMotionType(MovingView.MotionType.NONCONTINUOUS)
-        checkViewPosition(view, Position(5, 10))
-        assertTrue(view.paused)
-        assertEquals(MovingView.MotionType.NONCONTINUOUS, view.motionType)
+        val positions = listOf(Position(1.0, 3.0), Position(0.6, 12.0))
+        withMockedDegrees(listOf(90)) {
+            withMockedNextDouble(parentWidth.toDouble(), parentHeight.toDouble(), positions) {
+                // start non cont
+                var view = createView(createMockContext(0, true))
+                view.forcePosition(parentWidth, parentHeight, 5, 10)
 
-        // change type
-        view.updateMotionType(MovingView.MotionType.LINEAR)
-        checkViewPosition(view, Position(5, 10))
-        assertTrue(view.paused)
-        assertEquals(MovingView.MotionType.LINEAR, view.motionType)
-        assertEquals(0, view.movementSize)
+                // same type
+                view.updateMotionType(MovingView.MotionType.NONCONTINUOUS)
+                checkViewPosition(view, Position(5, 10))
+                assertTrue(view.paused)
+                assertEquals(MovingView.MotionType.NONCONTINUOUS, view.motionType)
 
-        // update with movement size
-        view.updateMotionType(MovingView.MotionType.NONCONTINUOUS)
-        view.updateMotionType(MovingView.MotionType.LINEAR, 10)
-        assertTrue(view.paused)
-        assertEquals(MovingView.MotionType.LINEAR, view.motionType)
-        assertEquals(10, view.movementSize)
+                // change type
+                view.updateMotionType(MovingView.MotionType.LINEAR)
+                checkViewPosition(view, Position(5, 10))
+                assertTrue(view.paused)
+                assertEquals(MovingView.MotionType.LINEAR, view.motionType)
+                assertEquals(0, view.movementSize)
 
-        // start linear
-        view = createView(createMockContext(1, false, 5))
-        view.forcePosition(100, 100, 5, 10)
+                // change type with movement size
+                view.updateMotionType(MovingView.MotionType.NONCONTINUOUS)
+                view.updateMotionType(MovingView.MotionType.LINEAR, 10)
+                assertTrue(view.paused)
+                assertEquals(MovingView.MotionType.LINEAR, view.motionType)
+                assertEquals(10, view.movementSize)
 
-        // update same type
-        view.updateMotionType(MovingView.MotionType.LINEAR, 1)
-        checkViewPosition(view, Position(5, 10))
-        assertFalse(view.paused)
-        assertEquals(MovingView.MotionType.LINEAR, view.motionType)
-        assertEquals(5, view.movementSize)
+                checkViewPosition(view, Position(5, 10))
+                view.updatePosition(parentWidth, parentHeight, forceUpdate = true)
+                checkViewPosition(view, Position(5, 20))
 
-        // change type
-        view.updateMotionType(MovingView.MotionType.NONCONTINUOUS)
-        checkViewPosition(view, Position(5, 10))
-        assertFalse(view.paused)
-        assertEquals(MovingView.MotionType.NONCONTINUOUS, view.motionType)
+                // start linear
+                view = createView(createMockContext(1, false, 5))
+                view.forcePosition(parentWidth, parentHeight, 5, 10)
 
-        // update resets movement size
-        view.updateMotionType(MovingView.MotionType.LINEAR, 1)
-        checkViewPosition(view, Position(5, 10))
-        assertFalse(view.paused)
-        assertEquals(MovingView.MotionType.LINEAR, view.motionType)
-        assertEquals(1, view.movementSize)
+                // update same type
+                view.updateMotionType(MovingView.MotionType.LINEAR, 1)
+                checkViewPosition(view, Position(5, 10))
+                assertFalse(view.paused)
+                assertEquals(MovingView.MotionType.LINEAR, view.motionType)
+                assertEquals(5, view.movementSize)
+
+                // change type
+                view.updateMotionType(MovingView.MotionType.NONCONTINUOUS)
+                checkViewPosition(view, Position(5, 10))
+                assertFalse(view.paused)
+                assertEquals(MovingView.MotionType.NONCONTINUOUS, view.motionType)
+
+                view.updatePosition(parentWidth, parentHeight, forceUpdate = true)
+                checkViewPosition(view, positions[0])
+
+                // update resets movement size
+                view.updateMotionType(MovingView.MotionType.LINEAR, 1)
+                checkViewPosition(view, positions[0])
+                assertFalse(view.paused)
+                assertEquals(MovingView.MotionType.LINEAR, view.motionType)
+                assertEquals(1, view.movementSize)
+            }
+        }
     }
 
     @Test
@@ -104,8 +125,8 @@ abstract class AbstractMovingViewTest<T> (private val createView: (Context) -> T
 
     @Test
     fun testSetInitialPosition() {
-        runNonContinuousForcePositionTests(createView)
-        runLinearForcePositionTests(createView)
+        runNonContinuousSetInitialPositionTests(createView)
+        runLinearSetInitialPositionTests(createView)
     }
 
     @Test
